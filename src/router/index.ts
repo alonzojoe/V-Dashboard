f084cc35d4292a21d5670b5f25c9ea1fa6fcecb2
Joenell Canlas Alonzo <72643848+alonzojoe@users.noreply.github.com>
2023-10-27 16:45:44 +0800

    Fixe Navbar and Sidebar w/ Hover

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,7 +16,7 @@ const routes = [
                 path: '',
                 name: 'dashboard',
                 component: () => import('@/pages/Dashboard/Dashboard.vue'),
-            }
+            },
 
         ]
     }
